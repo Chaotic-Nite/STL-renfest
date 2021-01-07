222966ae7df6673c37c402809c67741a17972822
Noel <chaoticforbidden@gmail.com>
2021-01-07 11:54:21 -0600

    setting up nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/renaissance-festival-logo.png";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/navigation/Navigation";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} />
       </header>
+      <Navigation />
+      <nav>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </nav>
     </div>
   );
 }
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,32 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import NavBar from "react-bootstrap/Navbar";
 import NavItem from "react-bootstrap/NavItem";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
 
 function Navigation() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+
+  const handleClose = () => setOpen(false);
+
   return (
     <NavBar bg="dark" variant="dark">
       <Nav className="navbar sticky-top" id="paths">
         <ul className="Nav-NavLinks">
+          <NavDropdown
+            title="Dropdown"
+            onMouseEnter={handleOpen}
+            onMouseLeave={handleClose}
+            open={open}
+            noCaret
+          >
+            <li>
+              <NavLink to="/tickets">Tickets</NavLink>
+            </li>
+          </NavDropdown>
           <NavItem>
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink to="/weddings">Weddings</NavLink>
             </li>
           </NavItem>
-          <NavItem>
+          <NavDropdown
+            title="Dropdown"
+            onMouseEnter={handleOpen}
+            onMouseLeave={handleClose}
+            open={open}
+            noCaret
+          >
             <li>
-              <NavLink to="/Photos">Photos</NavLink>
+              <NavLink to="/weekends">Themed Weekends</NavLink>
             </li>
-          </NavItem>
-          <NavItem>
+          </NavDropdown>
+          <NavDropdown
+            title="Dropdown"
+            onMouseEnter={handleOpen}
+            onMouseLeave={handleClose}
+            open={open}
+            noCaret
+          >
             <li>
-              <NavLink to="/Photos">Photos</NavLink>
+              <NavLink to="/entertain">Entertainers / Vendors</NavLink>
             </li>
-          </NavItem>
+          </NavDropdown>
+          <NavDropdown
+            title="Dropdown"
+            onMouseEnter={handleOpen}
+            onMouseLeave={handleClose}
+            open={open}
+            noCaret
+          >
+            <li>
+              <NavLink to="/cast">Cast</NavLink>
+            </li>
+          </NavDropdown>
           <NavItem>
             <li>
-              <NavLink to="/Photos">Photos</NavLink>
+              <NavLink to="/about-us">About Us</NavLink>
             </li>
           </NavItem>
         </ul>
