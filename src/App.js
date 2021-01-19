ae7d67870ecb04358d136bb1ed9894d2502b18af
Noel <chaoticforbidden@gmail.com>
2021-01-19 15:33:02 -0600

    Set up background and adjust onto the header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} />
+        <img id="logo" src={logo} alt="" />
       </header>
       <Navigation />
       <nav>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 function Home() {
+  const [state, setState] = useState(null);
+
   return <h1>Test</h1>;
 }
 
