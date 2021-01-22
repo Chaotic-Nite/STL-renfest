97b317ddd60617291fa3f4e53367dd30b9336812
Noel <chaoticforbidden@gmail.com>
2021-01-21 23:07:41 -0600

    Nav issues

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,53 +1,57 @@
 import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
 import "../../css/navigation.css";
+import { Dropdown } from "bootstrap";
 
 function Navigation() {
-  const [open, setOpen] = useState(false);
+  const [show, setShow] = useState(false);
+  const navStyles = {
+    color: "white",
+  };
+  const showDropdown = (e) => {
+    setShow(!show);
+  };
+  const hideDropdown = (e) => {
+    setShow(false);
+  };
 
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" class="navbar" variant="dark">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="m-auto navItems">
-            <Nav.Link className="navPiece" href="/">
-              Tickets
-            </Nav.Link>
-            <Nav.Link className="navPiece" href="/">
-              Weddings
-            </Nav.Link>
-            <Nav.Link className="navPiece" href="/">
-              Entertainers &#38; Vendors
-            </Nav.Link>
-            <Nav.Link className="navPiece" href="/">
-              Cast
-            </Nav.Link>
-            <Nav.Link className="navPiece" href="/about-us">
-              About Us
-            </Nav.Link>
-
-            {/* React Dropdown
-            <NavDropdown
-              className="navPiece"
-              title="Dropdown"
-              id="collasible-nav-dropdown"
-            >
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>*/}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Nav
+        fill
+        variant="tabs"
+        expand="lg"
+        class="navbar"
+        defaultActiveKey="/home"
+      >
+        <Nav.Link className="navPiece" style={navStyles} href="/">
+          Tickets
+        </Nav.Link>
+        <Nav.Link className="navPiece" style={navStyles} href="/">
+          Weddings
+        </Nav.Link>
+        <Nav.Link className="navPiece" style={navStyles} href="/">
+          Entertainers &#38; Vendors
+        </Nav.Link>
+        <Nav.Link className="navPiece" style={navStyles} href="/">
+          Cast
+        </Nav.Link>
+        <Nav.Link className="navPiece" style={navStyles} href="/about-us">
+          About Us
+        </Nav.Link>
+        <Dropdown
+          as={NavItem}
+          show={show}
+          onMouseEnter={showDropdown}
+          onMouseLeave={hideDropdown}
+        >
+          <Dropdown.Toggle as={NavLink}>Click to see more…</Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>Hello there!</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Nav>
     </>
   );
 }
