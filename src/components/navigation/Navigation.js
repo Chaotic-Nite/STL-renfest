701577c73cc1e9fa50b28e51ee6f93c6cd35668d
NoelKling <chaoticforbidden@gmail.com>
2021-03-04 18:28:19 -0600

    Resetting up navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,91 +1,37 @@
-import React, { useState } from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import React from "react";
 import "../../css/navigation.css";
 
 function Navigation() {
   return (
     <>
-      <nav className="nav">
-        <ul className="nav__menu">
-          <li className="nav__menu-item">
-            <a>Tickets</a>
-            <SubTickets />
+       {/*Navigation Bar with Dropdown Quick Links */}
+        <nav  className="nav">
+          <ul className="nav__menu" id="nav-bar">
+            <li><a className="small-tab drop" href="pages/tickets.html">Tickets</a>
+            <ul className="nav__submenu">
+              <li><a className="nav__submenu-item" href="/pages/tickets.html#general-tickets">Renaissance Tickets</a></li>
+              <li><a className="nav__submenu-item" href="/pages/tickets.html#les-amis">Les Amis Tickets</a></li>
+              <li><a className="nav__submenu-item" href="/pages/tickets.html#kegs">Kegs N Eggs Tickets</a></li>
+              <li><a className="nav__submenu-item" href="/pages/bring-your-dog.html">Bring Your Dog</a></li>
+            </ul>
           </li>
-          <li className="nav__menu-item">
-            <a>Weddings</a>
-          </li>
-          <li className="nav__menu-item">
-            <a>Themed Weekends</a>
-            <SubWeekends />
-          </li>
-          <li className="nav__menu-item">
-            <a>Enterainers &#38; Vendors</a>
-          </li>
-          <li classNam="nav__menu-item">
-            Cast
-            <SubCast />
-          </li>
-          <li classNam="nav__menu-item">
-            About Us
-          </li>
-        </ul>
-      </nav>
+            <li><a className="small-tab" href="pages/weddings.html">Weddings</a></li>
+            <li><a id="weekends" className="small-tab" href="pages/weekends.html">Themed Weekends</a></li>
+            <li>
+              <a id="entertain" href="pages/entertainment-vendor.html">Entertainers & Vendors</a>
+            </li>
+            <li>
+              <a className="small-tab drop" href="/pages/cast.html">Cast</a>
+              <ul className="nav__submenu">
+                <li><a className="nav__submenu-item" href="/pages/warriors-island.html">Warriors Island</a></li>
+              </ul>
+            </li>
+            <li><a className="small-tab" href="pages/about-us.html">About us</a></li>
+          </ul>
+        </nav>
     </>
   );
 }
 
-function SubTickets() {
-  return (
-    <ul className="nav__submenu">
-      <li className="nav__submenu-item ">
-        <a>Renaissance Tickets</a>
-      </li>
-      <li className="nav__submenu-item ">
-        <a>Les Amis Tickets</a>
-      </li>
-      <li className="nav__submenu-item ">
-        <a>Kegs N Eggs Tickets</a>
-      </li>
-      <li className="nav__submenu-item ">
-        <a>Bring Your Dog</a>
-      </li>
-    </ul>
-  );
-}
-
-function SubCast() {
-  return (
-    <ul className="nav__submenu">
-      <li className="nav__submenu-item ">
-        <a>Warriors Island</a>
-      </li>
-    </ul>
-  );
-}
-
-function SubWeekends() {
-  return (
-    <ul className="nav__submenu">
-      <li className="nav__submenu-item ">
-        <a>Highland Fling</a>
-      </li>
-      <li className="nav__submenu-item ">
-        <a>Pets, Pirates &#38; Ale</a>
-      </li>
-      <li className="nav__submenu-item ">
-        <a>Shamrocks, Shenanigans &#38; Sweet Romance</a>
-      </li>
-      <li className="nav__submenu-item ">
-        <a>Viking Invasion &#38; Cosplay in the Kingdom</a>
-      </li>
-      <li className="nav__submenu-item ">
-        <a>Oktoberfest &#38; Other Wonders of the World</a>
-      </li>
-      <li className="nav__submenu-item ">
-        <a>Ladies Shopping Day</a>
-      </li>
-    </ul>
-  );
-}
 
 export default Navigation;
