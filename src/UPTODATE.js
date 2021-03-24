04738240782077017a982c48c53722b33c84cebd
Noel <chaoticforbidden@gmail.com>
2021-03-24 09:46:44 -0500

    commit before rebase

--- src/UPTODATE.js
+++ src/UPTODATE.js
@@ -2,8 +2,8 @@
 const year = 2021;
 
 // The start and end days
-const startDay = "September 19th";
-const endDay = "October 18th";
+const startDay = "September 18th";
+const endDay = "October 17th";
 
 // How long the Faire has been happening
 const yearOfFaire = year - 1998 + "rd";
