0adf580d595812462c6c5a437bd59b2108bbab3d
Noel <chaoticforbidden@gmail.com>
2021-05-25 22:16:39 -0500

    adjusted wedding css for mobile

--- src/components/social-media/SocialMedia.js
+++ src/components/social-media/SocialMedia.js
@@ -1,27 +1,30 @@
 import { SocialIcon } from "react-social-icons";
 
+
 const SocialMedia = () => {
   return (
     <div style={{ marginTop: "2%" }}>
       <SocialIcon
         url="https://www.facebook.com/STLRenFest/"
         network="facebook"
-        style={{ width: "5vh", height: "5vh" }}
-      />
+        className='socialLink'
+        />
       <SocialIcon
         url="https://twitter.com/stlrenfest"
         network="twitter"
-        style={{ margin: "0 2%", width: "5vh", height: "5vh" }}
-      />
+        className='socialLink'
+        style={{ margin: "0 2%"}}
+        />
       <SocialIcon
         url="https://www.pinterest.com/stlouisrenaissa/"
         network="pinterest"
-        style={{ marginRight: "2%", width: "5vh", height: "5vh" }}
-      />
+        className='socialLink'
+        style={{ marginRight: "2%"}}
+        />
       <SocialIcon
         url="https://www.instagram.com/stlrenfest/"
         network="instagram"
-        style={{ width: "5vh", height: "5vh" }}
+        className='socialLink'
       />
     </div>
   );
