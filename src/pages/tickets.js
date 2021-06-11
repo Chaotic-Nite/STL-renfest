e0ada9e6221a35c1468e5417ad1de6cdf8429254
NoelKling <chaoticforbidden@gmail.com>
2021-06-11 14:00:02 -0500

    added key-policies

--- src/pages/tickets.js
+++ src/pages/tickets.js
@@ -12,155 +12,11 @@ function Tickets() {
       <div className="margin-width">
         <div className=" two-col">
           <div className="col1-width">
-            <div id="general-tickets"></div>
             <div className="ticket-setup">
-              <h2>Ticket Options</h2>
-              <div className="ticket-boxes">
-                <div className="ren-faire">
-                  <ul className="ticket-ul">
-                    <li className="ren-gen">Gate Admissions</li>
-                    <li>
-                      <p className="t-title">Adults:</p>
-                      <p className="t-price">$19.95</p>
-                    </li>
-                    <li>
-                      <p className="t-title">Children (5-12 yrs):</p>
-                      <p className="t-price">$12.95</p>
-                    </li>
-                    <li>
-                      <p className="t-title">Senior/Student:</p>
-                      <p className="t-price">$16.95</p>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="ren-faire">
-                  <ul className="ticket-ul">
-                    <li className="ren-gen">Advanced Tickets Online</li>
-                    <li>
-                      <p className="t-title">Adults:</p>
-                      <p className="t-price">$16.95</p>
-                    </li>
-                    <li>
-                      <p className="t-title">Children (5-12 yrs):</p>
-                      <p className="t-price">$10.95</p>
-                    </li>
-                    <li className="purchase-bttn">
-                      <a
-                        className="rf-la-bttn"
-                        href="https://red.vendini.com/ticket-software.html?t=tix&e=e0ed81fb98a9796c57dded8c785150d5"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Order Here
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="ren-faire">
-                  <ul className="ticket-ul">
-                    <li className="ren-gen">Season Pass</li>
-                    <li>
-                      <p className="t-title">Adults:</p>
-                      <p className="t-price">$84.95</p>
-                    </li>
-                    <li>
-                      <p className="t-title">Children (5-12 yrs):</p>
-                      <p className="t-price">$49.95</p>
-                    </li>
-                    <li className="purchase-bttn">
-                      <a
-                        className="rf-la-bttn"
-                        href="https://red.vendini.com/ticket-software.html?t=tix&e=725c321030840c23d87d540b4b7d87c6"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Order Here
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <h4 style={{ color: "rgb(76, 0, 255)", marginTop: "0" }}>
-                <i>Children 4 and under get in for free!</i>
-              </h4>
-
-              <h4>
-                For Group Discounts of 10 or more: <br />
-              </h4>
-              <p className="ticket-contact">
-                <i>
-                  Please Contact <b>akuchar@renaissancefest.com</b> or call{" "}
-                  <b>636.639.5173</b>
-                </i>
-              </p>
-              <div id="les-amis"></div>
-              <h2>Les Amis Packages</h2>
-              <h3 id="le-amis-title">
-                <i>"Friends of the Festival"</i>
-              </h3>
-              <p>
-                Want free, guaranteed seating in our private garden?
-                <br />
-                Purchase one of our Les Amis packages for the festival!
-                <br />
-                Includes snacks, cold water and/or lemonade, private handicap
-                privies and more!
-              </p>
-              <div className="ticket-boxes">
-                <div className="les-amis">
-                  <ul className="les-ul">
-                    <li
-                      className="les-header"
-                      style={{
-                        backgroundColor: "rgb(235, 204, 30)",
-                        color: "rgb(15, 15, 15)",
-                      }}
-                    >
-                      Gold
-                    </li>
-                    <li className="les-price">$120</li>
-                    <li>1 Season Pass</li>
-                    <li>4 Complimentary Tickets</li>
-                    <li>1 Les Amis Shirt</li>
-                    <li>1 Les Amis Membership Medallion</li>
-                    <li>
-                      VIP Gold Parking <br />
-                      <i>*Please Note: VIP Parking is first come first serve</i>
-                    </li>
-                    <li className="purchase-bttn">
-                      <a
-                        className="rf-la-bttn"
-                        href="https://red.vendini.com/ticket-software.html?e=51229aba0cf522dd834c02295d2a94d9&t=tix"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Purchase Here
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="les-amis">
-                  <ul className="les-ul">
-                    <li className="les-header">Friends</li>
-                    <li className="les-price">$54.95</li>
-                    <li>2 Complimentary Tickets</li>
-                    <li>1 Les Amis Shirt</li>
-                    <li className="purchase-bttn">
-                      <a
-                        className="rf-la-bttn"
-                        href="https://red.vendini.com/ticket-software.html?t=tix&e=60fba7232c2125909c5e77cdb1e002c0"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Purchase Here
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+            <span id="general-tickets"> &nbsp;</span>
+              <div ><GeneralTicket /></div>
+            <span id="les-amis"> &nbsp; </span>
+              <div ><LesAmisTicket /></div>
               <h2>Corporate Partners</h2>
               <p>
                 <a href="https://red.vendini.com/ticket-software.html?t=tix&e=e0ed81fb98a9796c57dded8c785150d5">
@@ -168,35 +24,9 @@ function Tickets() {
                 </a>{" "}
                 and have your code ready to purchase tickets!
               </p>
-              <div id="kegs" className="kne">
-                <h2>Kegs 'N' Eggs Tickets</h2>
-                <div className="ren-faire">
-                  <ul className="ticket-ul">
-                    <li className="ren-gen">Kegs 'N' Eggs Prices</li>
-                    <li>
-                      <p className="t-title">Adults:</p>
-                      <p className="t-price">$30.00</p>
-                    </li>
-                    <li>
-                      <p className="t-title">Designated Driver:</p>
-                      <p className="t-price">$15.00</p>
-                    </li>
-                    <li>
-                      <p className="t-title">Dog:</p>
-                      <p className="t-price">$10.00</p>
-                    </li>
-                    <li>
-                      <a
-                        className="rf-la-bttn"
-                        href="https://red.vendini.com/ticket-software.html?t=tix&w=0cca8bf7e55835461a115685ac926ea3"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Purchase Here
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+            <span id="kegs"> &nbsp; </span>
+              <div  className="kne">
+               <KegsTicket />
               </div>
             </div>
           </div>
@@ -235,6 +65,202 @@ function Tickets() {
       </div>
     </>
   );
+}
+
+function GeneralTicket() {
+  return (
+    <>
+      <h2>Ticket Options</h2>
+      <div className="ticket-boxes">
+        <div className="ren-faire">
+          <ul className="ticket-ul">
+            <li className="ren-gen">Gate Admissions</li>
+            <li>
+              <p className="t-title">Adults:</p>
+              <p className="t-price">$19.95</p>
+            </li>
+            <li>
+              <p className="t-title">Children (5-12 yrs):</p>
+              <p className="t-price">$12.95</p>
+            </li>
+            <li>
+              <p className="t-title">Senior/Student:</p>
+              <p className="t-price">$16.95</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ren-faire">
+          <ul className="ticket-ul">
+            <li className="ren-gen">Advanced Tickets Online</li>
+            <li>
+              <p className="t-title">Adults:</p>
+              <p className="t-price">$16.95</p>
+            </li>
+            <li>
+              <p className="t-title">Children (5-12 yrs):</p>
+              <p className="t-price">$10.95</p>
+            </li>
+            <li className="purchase-bttn">
+              <a
+                className="rf-la-bttn"
+                href="https://red.vendini.com/ticket-software.html?t=tix&e=e0ed81fb98a9796c57dded8c785150d5"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Order Here
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ren-faire">
+          <ul className="ticket-ul">
+            <li className="ren-gen">Season Pass</li>
+            <li>
+              <p className="t-title">Adults:</p>
+              <p className="t-price">$84.95</p>
+            </li>
+            <li>
+              <p className="t-title">Children (5-12 yrs):</p>
+              <p className="t-price">$49.95</p>
+            </li>
+            <li className="purchase-bttn">
+              <a
+                className="rf-la-bttn"
+                href="https://red.vendini.com/ticket-software.html?t=tix&e=725c321030840c23d87d540b4b7d87c6"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Order Here
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <h4 style={{ color: "rgb(76, 0, 255)", marginTop: "0" }}>
+        <i>Children 4 and under get in for free!</i>
+      </h4>
+
+      <h4>
+        For Group Discounts of 10 or more: <br />
+      </h4>
+      <p className="ticket-contact">
+        <i>
+          Please Contact <b>akuchar@renaissancefest.com</b> or call{" "}
+          <b>636.639.5173</b>
+        </i>
+      </p>
+      </>
+  )
+}
+
+function LesAmisTicket() {
+  return (
+    <>
+    <h2>Les Amis Packages</h2>
+    <h3 id="le-amis-title">
+      <i>"Friends of the Festival"</i>
+    </h3>
+    <p>
+      Want free, guaranteed seating in our private garden?
+      <br />
+      Purchase one of our Les Amis packages for the festival!
+      <br />
+      Includes snacks, cold water and/or lemonade, private handicap
+      privies and more!
+    </p>
+    <div className="ticket-boxes">
+      <div className="les-amis">
+        <ul className="les-ul">
+          <li
+            className="les-header"
+            style={{
+              backgroundColor: "rgb(235, 204, 30)",
+              color: "rgb(15, 15, 15)",
+            }}
+          >
+            Gold
+          </li>
+          <li className="les-price">$120</li>
+          <li>1 Season Pass</li>
+          <li>4 Complimentary Tickets</li>
+          <li>1 Les Amis Shirt</li>
+          <li>1 Les Amis Membership Medallion</li>
+          <li>
+            VIP Gold Parking <br />
+            <i>*Please Note: VIP Parking is first come first serve</i>
+          </li>
+          <li className="purchase-bttn">
+            <a
+              className="rf-la-bttn"
+              href="https://red.vendini.com/ticket-software.html?e=51229aba0cf522dd834c02295d2a94d9&t=tix"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Purchase Here
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="les-amis">
+        <ul className="les-ul">
+          <li className="les-header">Friends</li>
+          <li className="les-price">$54.95</li>
+          <li>2 Complimentary Tickets</li>
+          <li>1 Les Amis Shirt</li>
+          <li className="purchase-bttn">
+            <a
+              className="rf-la-bttn"
+              href="https://red.vendini.com/ticket-software.html?t=tix&e=60fba7232c2125909c5e77cdb1e002c0"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Purchase Here
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+function KegsTicket() {
+  return (
+    <>
+      <h2>Kegs 'N' Eggs Tickets</h2>
+        <div className="ren-faire">
+          <ul className="ticket-ul">
+            <li className="ren-gen">Kegs 'N' Eggs Prices</li>
+            <li>
+              <p className="t-title">Adults:</p>
+              <p className="t-price">$30.00</p>
+            </li>
+            <li>
+              <p className="t-title">Designated Driver:</p>
+              <p className="t-price">$15.00</p>
+            </li>
+            <li>
+              <p className="t-title">Dog:</p>
+              <p className="t-price">$10.00</p>
+            </li>
+            <li>
+              <a
+                className="rf-la-bttn"
+                href="https://red.vendini.com/ticket-software.html?t=tix&w=0cca8bf7e55835461a115685ac926ea3"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Purchase Here
+              </a>
+            </li>
+          </ul>
+        </div>
+    </>
+  )
 }
 
 export default Tickets;
