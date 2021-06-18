a95fa06cc61a37ccd02d1fd156a3d248358d7e3e
NoelKling <chaoticforbidden@gmail.com>
2021-06-17 22:03:58 -0500

    added information and filled out themed weekends

---
+++ src/lists/themed-schedules.js
@@ -0,0 +1,189 @@
+
+export const pavilionSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
+
+export const stlouisSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
+
+export const owainSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
+
+export const friendshipSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
+
+export const joanOfArcSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
+
+export const marcoPoloSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
+
+export const storytellingSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
+
+export const knightsSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
+
+export const piratesSch = {
+    'highland-fling' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'pets-pirate-ale' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'shamrocks-shenanigans' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'viking-invasion' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'oktoberfest' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+    'festive-shopping-day' : [
+        {name: 'Coming Soon', time: '' },
+    ],
+}
