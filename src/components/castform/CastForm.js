9f7e371477dd3d1c52c92d9e210f3c58c890a332
Noel <Chaoticforbidden@gmail.com>
2022-03-21 21:22:15 -0500

    Added env for EmailJS, setting up Cast Inquiry

--- src/components/castform/CastForm.js
+++ src/components/castform/CastForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { MDBInput, MDBBtn } from "mdb-react-ui-kit"
+import Form from 'react-bootstrap/Form'
 import emailjs from "@emailjs/browser";
 import Swal from "sweetalert2";
 
@@ -8,19 +8,16 @@ import "react-datepicker/dist/react-datepicker.css";
 
 
 function ContactForm(props) {
-  const [wedDate, setWedDate] = useState(new Date());
 
   const [form, setForm] = useState({
     from_name: '',
     from_email: '',
-    date: wedDate,
-    subject: '',
     message: '',
   })
 
-  // const USER_ID = process.env.REACT_APP_USER_ID;
-  // const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
-  // const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+  const USER_ID = process.env.REACT_APP_USER_ID;
+  const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+  const TEMPLATE_ID = process.env.REACT_SPECIFIC_TEMPLATE_ID;
 
   const handleOnChange = (evt) => {
     setForm(prevForm => ({
@@ -46,29 +43,43 @@ function ContactForm(props) {
           text: error.text,
         })
       });
-    setWedDate(new Date());
     setForm({
       from_name: '',
       from_email: '',
-      date: wedDate,
-      subject: '',
       message: '',
     })
   };
 
   return (
     <>
-      <form id="contact-form" onSubmit={handleOnSubmit}>
-        <MDBInput required type='text' name="from_name" label="Your Name" value={form.from_name} size='lg'
-          onChange={(ev) => handleOnChange(ev)} />
-        <MDBInput required type='email' name="from_email" label="Your Email" value={form.from_email} size='lg'
-          onChange={(ev) => handleOnChange(ev)} />
-        <MDBInput required type='text' label="Subject" name="subject" value={form.subject} size='lg'
-          onChange={(ev) = npm i> handleOnChange(ev)} />
+      <Form id="contact-form" onSubmit={handleOnSubmit}>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Name: Francis I of France" value={form.from_name} onChange={(ev) => handleOnChange(ev)}/>
+          <Form.Text>Please put your name and quotations if you have a preferred name.</Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPronouns">
+          <Form.Label>Pronouns</Form.Label>
+          <Form.Control type="text" placeholder="He/Him" value={form.from_pronouns} onChange={(ev) => handleOnChange(ev)}/>
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Your Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter Email: KingOFrance@baguette.com" value={form.from_email} onChange={(ev) => handleOnChange(ev)}/>
+          <Form.Text>This will be the default method to reaching back out to you.</Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicContact">
+          <Form.Label>Preferred Form of Contact:</Form.Label>
+          <Form.Control type="text" placeholder="Carrier Pigeon, Messenger, Via Text" value={form.from_altcontact} onChange={(ev) => handleOnChange(ev)}/>
+          <Form.Text>If you prefer to be contacted other than email, place here.</Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicContact">
+          <Form.Label>Preferred Form of Contact:</Form.Label>
+          <Form.Control as="textarea" rows={5} maxLength="1500" placeholder="Carrier Pigeon, Messenger, Via Text" value={form.message} onChange={(ev) => handleOnChange(ev)}/>
+        </Form.Group>
           <MDBInput textarea label="Your Message" name="message" value={form.message} rows="5" size='lg'
             onChange={(ev) => handleOnChange(ev)} />
           <MDBBtn type="submit">Submit</MDBBtn>
-      </form>
+      </Form>
     </>
   )
 }
